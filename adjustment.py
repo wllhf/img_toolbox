ce8805fdc59bb38f012dbf8f91ccc02ecf38494c
M. Woellhaf <manuelwoellhaf@gmail.com>
2017-02-13 18:11:58 +0100

    Added adjustment.crop and adjustment.crop_random.

--- adjustment.py
+++ adjustment.py
@@ -38,3 +38,78 @@ def per_image_standardization(images):
     images = images - mean
     images = images / np.maximum(std, 1.0 / np.prod(images.shape[1:]))
     return images
+
+
+def crop(images, size, coords=None):
+    """ Crops images.
+
+    Parameters:
+    -----------
+    images: numpy array (n, k, l, c)
+      Images.
+    size: array like (2,)
+      Size of new images [u, v].
+    coords: array like (2,) (default: None)
+      Center coordinates of resulting images. Defaults to center of original images.
+
+    Return:
+    -------
+    images: numpy array (n, u, v, c)
+
+    Raises:
+    -------
+    ValueError: if shape of images, coords or size is incompatible with this function or with each other.
+    """
+    coords = np.array(images.shape[1:3])//2 if coords is None else np.array(coords)
+    size = np.array(size)
+
+    if len(images.shape) != 4:
+        raise ValueError("given image shape is not compatible (!=4)")
+    if size.shape[0] != 2:
+        raise ValueError("given size is not compatible (!=2)")
+    if coords is not None and coords.shape[0] != 2:
+        raise ValueError("given coords are not compatible (!=2)")
+
+    s = coords - size//2
+    e = coords + (size-1)//2 + 1
+
+    valid = ((0, 0) <= s) * (e <= images.shape[1:3])
+    if not valid.all():
+        raise ValueError("given images, coords and size incompatible")
+
+    return images[:, s[0]:e[0], s[1]:e[1], :]
+
+
+def crop_random(images, size, ncrops=1):
+    """ Crops images at random center coordinates.
+
+    Parameters:
+    -----------
+    images: numpy array (n, k, l, c)
+      Images.
+    size: array like (2,)
+      Size of new images [u, v].
+    ncrops: int (default: 1)
+      If > 1 multiple stacked crops are returned
+
+    Return:
+    -------
+    images: numpy array (n*ncrops, u, v, c)
+
+    Raises:
+    -------
+    ValueError: if shape of images, coords or size is incompatible with this function or with each other.
+    """
+    size = np.array(size)
+    s = size//2
+    e = images.shape[1:3] - (size-1)//2 - 1
+
+    y = np.random.random_integers(s[0], e[0], size=ncrops)
+    x = np.random.random_integers(s[1], e[1], size=ncrops)
+    coords = np.stack([y, x], axis=1)
+
+    new = []
+    for c in coords:
+        new.append(crop(images=images, size=size, coords=c))
+
+    return np.concatenate(new, axis=0)
